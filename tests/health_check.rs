887f61d24592438d07b8871c7a5a985357ecafb6
stvsmth <stvsmth@gmail.com>
2022-03-20 13:50:23 -0600

    Allow for random ports in tests.

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,14 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request");
@@ -16,7 +18,11 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind to address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+    let port = listener.local_addr().unwrap().port();
+    println!("port: {:?}", port);
+    let server = zero2prod::run(listener).expect("Failed to bind to address");
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
